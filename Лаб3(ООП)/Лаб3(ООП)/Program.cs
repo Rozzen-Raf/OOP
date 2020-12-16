19c24b2f4f8ecf0257c71045eb3b156942d7c8e1
Rozzen-Raf <76116092+Rozzen-Raf@users.noreply.github.com>
2020-12-16 18:32:52 +0300

    Add files via upload

---
+++ Лаб3(ООП)/Лаб3(ООП)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Лаб3_ООП_
+{
+    
+    class Program
+    {
+       static int average(matrix mat, int m, int n)
+        {
+            int aver=0;
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; ++j)
+                {
+                    aver += mat.getel(i, j);
+                }
+            }
+            aver /= m * n;
+            return aver;
+            
+        }
+        static matrix changer(matrix mat,int m,int n,int aver)
+        {
+            for(int i = 0; i < m; ++i)
+            {
+                for(int j = 0; j < n; ++j)
+                {
+                    if (mat.getel(i, j) > aver)
+                    {
+                        mat.changemat(i, j, -1);
+                    }
+                }
+            }
+            return mat;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размеры матрицы MxN:");
+            int m, n;
+            m = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+            matrix mat = new matrix(m, n);
+            mat.show();
+            int aver = average(mat, m, n);
+           
+            Console.WriteLine("Среднее арифметическое элементов матрицы: {0}",aver);
+
+            mat = changer(mat, m, n, aver);
+            mat.show();
+        }
+    }
+}
---
+++ Лаб3(ООП)/Лаб3(ООП)/matrix.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Лаб3_ООП_
+{
+    class matrix
+    {
+        List<List<int>> mat = new List<List<int>>();
+        int m;
+        int n;
+       public matrix(int m,int n)
+        {
+            this.m = m;
+            this.n = n;
+            for(int i = 0; i < m; ++i)
+            {
+                List<int> temp = new List<int>();
+                Random rnd = new Random();
+                for(int j=0;j<n;j++)
+                    temp.Add(rnd.Next() % 10);
+                mat.Add(temp);
+            }
+        }
+        public void show()
+        {
+            Console.WriteLine("Вывод матрицы");
+            for(int i = 0; i < this.m; ++i)
+            {
+                for(int j = 0; j < this.n; ++j)
+                {
+                    Console.Write(String.Format("{0,7:000}", mat[i][j] + " "));
+                }
+                Console.Write("\n");
+            }
+           
+        }
+        public int getel(int in1,int jn)
+        {
+            if (in1 < this.m && jn < this.n)
+                return mat[in1][jn];
+            else
+            {
+                Console.WriteLine("Выход за пределы матрицы");
+                return -2;
+            }
+        }
+        public void changemat(int in1,int jn, int num)
+        {
+            if (in1 < this.m && jn < this.n)
+            {
+                mat[in1][jn] = num;
+
+            }
+            else
+            {
+                Console.WriteLine("Выход за пределы матрицы");
+            }
+        }
+    }
+}
